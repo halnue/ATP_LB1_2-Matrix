386fc471797f37dd4e678e4ffe02a58bd123c786
orueI <45312088+orueI@users.noreply.github.com>
2020-05-11 13:45:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include "Matrix.cpp"
-//#include "Matrix.h"
-#include "ListInt.cpp"
-#include "List.cpp"
 
-//Matrix changeNum(Matrix m);
-#define PRINT_MATRIX(variable) std::cout << std::string(#variable) << ": "<< std::endl<<variable << std::endl;
+#define PRINT_MATRIX(variable) std::cout << std::string(#variable) << ": "<< std::endl<<variable << std::endl; // todo remove
 
+using namespace std;
+
+Matrix changeNum(Matrix m);
 
 int main() {
     int **m1 = new int *[2];
@@ -16,18 +15,33 @@ int main() {
     m2[0] = new int[2]{8, 1};
     m2[1] = new int[2]{8, 5};
 
-    Matrix matrix1 = Matrix(2, 2, List(2, 2, m1));
-    Matrix matrix2 = Matrix(2, 2, List(2, 2, m2));
+    Matrix matrix1 = Matrix(2, 2, m1);
+    Matrix matrix2 = Matrix(2, 2, m2);
 
     PRINT_MATRIX(matrix1)
     PRINT_MATRIX(&matrix2)
     Matrix matrix3 = changeNum(matrix1);
     PRINT_MATRIX(matrix3)
 
-    Matrix matrixSum = matrix1.plus(matrix2);
-    PRINT_MATRIX( matrixSum )
-    PRINT_MATRIX(matrix1.subtraction(matrix2))
-    PRINT_MATRIX( matrix1 * matrix2 )
+    PRINT_MATRIX(matrix1 + matrix2)
+    PRINT_MATRIX(matrix1 - matrix2)
+    PRINT_MATRIX(matrix1 * matrix2)
 
     return 0;
+}
+
+Matrix changeNum(Matrix m) {
+    int w = m.getWidth();
+    int h = m.getHeight();
+    Matrix matrix = Matrix(m.getWidth(), m.getHeight());
+    for (int x = 0; x < w; ++x) {
+        for (int y = 0; y < h; ++y) {
+            int num = m[x][y];
+            if (num == 0)
+                matrix[x][y] = 1;
+            else
+                matrix[x][y] = num;
+        }
+    }
+    return matrix;
 }
